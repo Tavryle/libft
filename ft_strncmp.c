f29fd26dcb77e9b527f0344c1ee49b2526496362
Tavryle MANSINGH <tmansing@c4r7s10.wethinkcode.co.za>
2019-05-24 09:44:42 +0200

    better

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,29 @@
 /*   By: tmansing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 12:50:44 by tmansing          #+#    #+#             */
-/*   Updated: 2019/05/23 14:48:32 by tmansing         ###   ########.fr       */
+/*   Updated: 2019/05/24 08:55:27 by tmansing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int ft_strncmp(char *s1, char *s2, size_t n)
 {
 	int i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
 		i++;
-	return (s1[i] - s2[i]);
+	return (*s1 - *s2);
 }
 
 int	main(void)
 {
-	char s1[] = 
+	char s1[] = "breen";
+	char s2[] = "dlue";
+	size_t i;
+
+	i = '3';
+	printf("%d\n", ft_strncmp(s1, s2, i));
+	return (0);
+}
