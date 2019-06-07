4ba3ce63051e08d0c540e9ae14a2bed2cbabfb5c
Tavryle MANSINGH <tmansing@c4r7s10.wethinkcode.co.za>
2019-06-07 15:49:33 +0200

    correct

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,28 +6,29 @@
 /*   By: tmansing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 10:40:32 by tmansing          #+#    #+#             */
-/*   Updated: 2019/06/05 13:18:47 by tmansing         ###   ########.fr       */
+/*   Updated: 2019/06/07 13:46:36 by tmansing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t        ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
-    size_t d_len;
-    size_t s_len;
-    size_t i;
+	size_t i;
+	size_t len;
+	size_t k;
 
-    d_len = (size_t)ft_strlen((char *)dst);
-    s_len = (size_t)ft_strlen((char *)src);
-    i = d_len;
-    while ((*src) && ((d_len + 1) < size))
-    {
-        dst[d_len++] = *src++;
-    }
-    dst[d_len] = '\0';
-    if (i > size)
-        return (s_len + size);
-    else
-        return (s_len + i);
+	i = 0;
+	len = ft_strlen((const char *)dst);
+	k = len;
+	if (len > n)
+		k = n;
+	k += ft_strlen(src);
+	while (src[i] != '\0' && i + len < n - 1 && n > 0)
+	{
+		dst[i + len] = src[i];
+		i++;
+	}
+	dst[i + len] = '\0';
+	return (k);
 }
